5ae58a334f95158b828a70e97e8917acfd563995
Aşkın Özgür <askin@askin.ws>
2019-09-24 00:19:38 +0300

    Fixed: forgotten params to mail function Fix indentation Remove unused imports

--- hermes_notify.py
+++ hermes_notify.py
@@ -2,37 +2,34 @@
 
 import smtplib
 from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 # from email import Encoders
-import os
-import datetime
 
 def mail(to, subject, text, email_config, html=None):
-   msg = MIMEMultipart()
-   msg['From'] = email_config['mail_user']
-   msg['To'] = to
-   msg['Subject'] = subject
+    msg = MIMEMultipart()
+    msg['From'] = email_config['mail_user']
+    msg['To'] = to
+    msg['Subject'] = subject
 
-   if html is None:
-      msg.attach(MIMEText(text))
-   else:
-      msg.attach(MIMEText(html, 'html'))
+    if html is None:
+        msg.attach(MIMEText(text))
+    else:
+        msg.attach(MIMEText(html, 'html'))
 
-   mailServer = smtplib.SMTP(email_config['smtp'], email_config['smtp_port'])
-   mailServer.ehlo()
-   mailServer.ehlo()
-   mailServer.starttls()
-   mailServer.login(email_config['mail_user'], email_config['mail_pwd'])
-   mailServer.sendmail(email_config['mail_user'], to, msg.as_string())
-   # Should be mailServer.quit(), but that crashes...
-   mailServer.close()
+    mailServer = smtplib.SMTP(email_config['smtp'], email_config['smtp_port'])
+    mailServer.ehlo()
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.login(email_config['mail_user'], email_config['mail_pwd'])
+    mailServer.sendmail(email_config['mail_user'], to, msg.as_string())
+    # Should be mailServer.quit(), but that crashes...
+    mailServer.close()
 
 def warn_html(subject, content, email_config):
     for address in email_config['to_address']:
-        mail(address,subject,content, html=content)
+        mail(address, subject, content, email_config, html=content)
 
 
 def warn(subject, content, email_config):
     for address in email_config['to_address']:
-        mail(address,subject,content)
+        mail(address, subject, content, email_config)
